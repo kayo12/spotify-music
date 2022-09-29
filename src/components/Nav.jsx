50e2137a16af3d5c214919b7e384df90c05ca244
Kayo campos <kayonbahenrique@gmail.com>
2022-09-29 01:18:46 -0300

    Update: alinhando navbar deixando icones responsivos

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,27 +1,30 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react';
-import './Nav.css' 
-import { Link } from 'react-router-dom'
-import Logo from '../assests/img/logo-dimond.png'
-export default props => 
-    <nav className="nav">
-        <div className="nav-logo">
-            <span id="logo">Dimond Music</span>
-            <img src={Logo} alt="" />
-        </div>
-        <div className="nav-itens">
-            <div className="hidden-icon">
-                    <i className="fa fa-bars"></i>
-            </div>
-            <Link to='/'>
-                <i className="fa fa-home" title="Home" ></i>
-            </Link>
-            <Link to="/player">
-                <i className="fa fa-music" title="Musicas" ></i>
-            </Link>
-            <Link to="/mylist">
-                <i className="fa fa-list" title="Minha Lista"></i>
-            </Link>
-
-        </div>    
-    </nav>
+import React from "react";
+import "./Nav.css";
+import { Link } from "react-router-dom";
+import Logo from "../assests/img/logo-dimond.png";
+export default (props) => (
+  <nav className="nav">
+    <div className="nav-logo">
+      <span id="logo">Dimond Music</span>
+      <img src={Logo} alt="" />
+    </div>
+    <div className="nav-itens">
+     
+      <div className="open-icon">
+        <Link to="/">
+          <i className="fa fa-home" title="Home"></i>
+        </Link>
+        <Link to="/player">
+          <i className="fa fa-music" title="Musicas"></i>
+        </Link>
+        <Link to="/mylist">
+          <i className="fa fa-list" title="Minha Lista"></i>
+        </Link>
+      </div>
+      <div className="hidden-icon">
+        <button className="fa fa-bars"></button>
+      </div>
+    </div>
+  </nav>
+);
